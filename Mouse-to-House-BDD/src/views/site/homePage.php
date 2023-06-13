2ef818d3693a405e2531e8943ea94ba5ecfd40b0
AlexMargotCPNV <99970054+AlexMargotCPNV@users.noreply.github.com>
2023-06-14 00:13:03 +0200

    fin de la page home
    la homePage s'affiche desormais comme il se doit

--- Mouse-to-House-BDD/src/views/site/homePage.php
+++ Mouse-to-House-BDD/src/views/site/homePage.php
@@ -1,28 +1,23 @@
-<?php $title = "Accueil";
+<?php
+$title = "Accueil";
 echo "<h1 class='pageTitle' style='font-size: 45px'>Accueil</h1>";
 
-
 $mouse = $bag['data']['mouse'];
-print_r($bag['data']['mouse']);
 
-    if (!empty($mouse)) {
 // Afficher les informations du produit choisi aléatoirement
-        if (isset($mouse['data']['mouse']['image_path']) && isset($mouse['data']['mouse']['brand']) && isset($mouse['data']['mouse']['model']) && isset($mouse['data']['mouse']['price_francs'])) {
-            ?>
-            <p id="hometext">La <b><?php echo $mouse['data']['mouse']['model'] ?></b> de chez <b><?php echo $mouse['data']['mouse']['brand'] ?></b>
-                est
-                disponible au prix de <strong><?php echo $mouse['data']['mouse']['price_francs'] ?> CHF !</strong></p>
-        <a id="details" href="item/<?= $mouse['data']['mouse']['code'] ?>">
-            <img class="HomePageImg" src="<?php echo $mouse['data']['mouse']['image_path']; ?>" alt="Image de la souris">
-            </a><?php
-            ?>
-            <a id="lienproduits" href="userItems">Découvrez tous nos produits ici !</a><?php
-
-        } else {    // Sinon afficher un message d'erreur
-            echo "<br>Article non trouvé";
-        }
-
-    } else { //si le tableau "mouse" est vide affichage d'une erreur
-        echo "une erreur est survenue, essayer de relancer la page, si l'erreur persiste, merci de nous envoyer un message au traver de la page d'aide";
-    }
+if (isset($mouse[0]['image_path']) && isset($mouse[0]['brand']) && isset($mouse[0]['model']) && isset($mouse[0]['price_francs'])) {
+    ?>
+    <p id="hometext">La <b><?php echo $mouse[0]['model'] ?></b> de chez <b><?php echo $mouse[0]['brand'] ?></b>
+        est
+        disponible au prix de <strong><?php echo $mouse[0]['price_francs'] ?> CHF !</strong></p>
+    <a id="details" href="item/<?= $mouse[0]['code'] ?>">
+        <img class="HomePageImg" src="<?php echo $mouse[0]['image_path']; ?>" alt="Image de la souris">
+    </a>
+    <?php
+    ?>
+    <a id="lienproduits" href="userItems">Découvrez tous nos produits ici !</a>
+    <?php
+} else {    // Sinon afficher un message d'erreur
+    echo "<br>Article non trouvé";
+}
 ?>
