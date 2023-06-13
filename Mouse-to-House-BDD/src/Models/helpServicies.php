e7159c96feadc686953c837899a028750c19a028
EvannMartins <Evann.MARTINS@cpnv.ch>
2023-06-13 15:54:51 +0200

    Fonction help

---
+++ Mouse-to-House-BDD/src/Models/helpServicies.php
@@ -0,0 +1,13 @@
+<?php
+
+
+require_once 'dbConnector.php';
+
+function sendHelp($help)
+{
+    $helpQuery = 'INSERT ';
+
+    $Description = $_POST['Description'];
+
+
+}
--- Mouse-to-House-BDD/src/views/site/helppage.php
+++ Mouse-to-House-BDD/src/views/site/helppage.php
@@ -3,9 +3,9 @@
     <?php
     // Si un utilisateur n'est pas connecté alors renvoyer l'utilisateur à la page de connexion
     if(!isset($_SESSION['auth'])) {
-        echo '<form style="box-shadow: none;margin-top: 50px" action="login" id="formSignaler" method="post">';
+        echo '<form style="box-shadow: none;margin-top: 50px" href="login" id="formSignaler" method="post">';
     } else {    // Sinon envoyer le message de demande de l'aide
-        echo '<form style="box-shadow: none;margin-top: 50px" action="helpSend" id="formSignaler" method="post">';
+        echo '<form style="box-shadow: none;margin-top: 50px" action="help" id="formSignaler" method="post">';
     }
     ?>
         <label id="Problème"><h4>Avez-vous un problème ?</h4></label><br>
@@ -14,7 +14,7 @@
         <textarea type="text" id="Description" name="DescriptionArea"></textarea>
         <br>
         <br>
-        <input type="submit" id='signaler' value='Signaler le problème'>
+        <input type="submit" id='signaler' value='Signaler le problème' required>
 </div>
 <br><br><br>
 <div id="DivContact">
