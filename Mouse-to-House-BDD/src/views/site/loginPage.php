7adbff0d3d49526d1f97a113785ec5df940cfa44
MARGOT Alex <Alex.MARGOT@cpnv.ch>
2023-06-14 16:52:48 +0200

    loginpage

--- Mouse-to-House-BDD/src/views/site/loginPage.php
+++ Mouse-to-House-BDD/src/views/site/loginPage.php
@@ -5,15 +5,15 @@
     <form action="/login" method="POST">
         <h1>Connexion</h1>
 
-        <label><b>E-mail</b></label>
-        <input type="text" placeholder="Entrer l'E-mail" name="email" required>
+        <label><b>Username</b></label>
+        <input type="text" placeholder="Entrer votre username" name="username" required>
 
         <label><b>Mot de passe</b></label>
-        <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+        <input type="password" placeholder="Entrer votre mot de passe" name="password" required>
 
         <input type="submit" id='submit' value='Se connecter'>
         <?php if ($bag['loginFailed'] == 2) : ?>
-            <h5><span style="color:red">E-mail ou mot de passe incorrect!</span></h5>
+            <h5><span style="color:red">Username ou mot de passe incorrect!</span></h5>
         <?php  endif ?>
 
     </form>
