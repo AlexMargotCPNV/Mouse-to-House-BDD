57ce9742ea89052d6225e0ff5a1c396bb8fa18f8
EvannMartins <Evann.MARTINS@cpnv.ch>
2023-06-15 22:11:12 +0200

    Finish profilSearch

--- Mouse-to-House-BDD/src/Models/profilSearch.php
+++ Mouse-to-House-BDD/src/Models/profilSearch.php
@@ -6,9 +6,8 @@ require_once 'dbConnector.php';
 function userInformation(){
 
     $userRequesting = $_SESSION['current_user'];
-    $separator = "\"";
 
-    $profilQuery = "SELECT username, surname, firstname, email FROM mth.users WHERE username = ($userRequesting)";
+    $profilQuery = "SELECT username, surname, firstname, email FROM mth.users WHERE username = ('$userRequesting')";
 
     return executeQuerySelect($profilQuery);
 
